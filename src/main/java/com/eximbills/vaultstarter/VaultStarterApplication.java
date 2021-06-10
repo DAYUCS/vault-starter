43bc704d4169cefabb9c7f0b52801dc53299e7ea
Dayu Bian <dayub@chinasystems.com>
2021-06-10 12:30:09 +0800

    init

---
+++ src/main/java/com/eximbills/vaultstarter/VaultStarterApplication.java
@@ -0,0 +1,13 @@
+package com.eximbills.vaultstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultStarterApplication.class, args);
+	}
+
+}
