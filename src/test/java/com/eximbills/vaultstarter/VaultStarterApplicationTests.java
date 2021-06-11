787a9f9a0665c615e307744aa783c058c5ec15b1
Dayu Bian <dayub@chinasystems.com>
2021-06-11 08:35:32 +0800

    update

---
+++ src/test/java/com/eximbills/vaultstarter/VaultStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eximbills.vaultstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
